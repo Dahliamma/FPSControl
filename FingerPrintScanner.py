7b3ae96cc6220c2cebac9351110121bdcf7aeb6a
Mohamed Abdel-Hafiz <dahliamma@gmail.com>
2017-03-13 15:25:39 -0600

    initial commit

---
+++ FingerPrintScanner.py
@@ -0,0 +1,17 @@
+"""
+Fingerprint Class
+"""
+
+class FingerPrintScanner():
+    """
+    FingerPrintScanner Class
+    Responsible for scanning fingerprint, reporting scan status to other classes
+
+    Args:
+    """
+
+    def __init__(self):
+        self._status = 0;
+        self.initialize
+
+    def initialize(self):
