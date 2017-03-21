dd3f6a772f2798c6b70c7a717fae544eb9c3a693
Mohamed Abdel-Hafiz <dahliamma@gmail.com>
2017-03-20 20:05:16 -0600

    added preliminary enrollment and identification code. Also cleaned up initialization a bit

--- FingerPrintScanner.py
+++ FingerPrintScanner.py
@@ -3,6 +3,7 @@ Fingerprint Class
 """
 import FPS as FPS
 print('Imported FPS')
+from collections import Counter
 #import time as time
 #print('Imported time')
 
@@ -17,26 +18,48 @@ class FingerPrintScanner():
     def __init__(self):
         self._status = 0
         print('Starting initialization')
-        self._image = None
-        self.initialize()
-
-    def initialize(self):
-        print('Begin')
-        fps = FPS.FPS_GT511C3(device_name='/dev/ttyAMA0', baud=9600, timeout=2, is_com=False)
-        fps.UseSerialDebug = False
+        self.fps = FPS.FPS_GT511C3(device_name='/dev/ttyAMA0', baud=9600, timeout=2, is_com=False)
+        self.fps.UseSerialDebug = False
         print('Scanner connected')
-        print(fps.IsPressFinger())
+        self._image = None
+        self.finger_test()
+        self._finger_number = None
+        self._finger_scan_number = None
+        self._collected_scans = None
+        self._true_scan_number = None
+
+    def finger_test(self):
+        print('Begin')
+        print(self.fps.IsPressFinger())
         FPS.delay(1)
-        fps.SetLED(True)
+        self.fps.SetLED(True)
         print('Place finger on scanner.')
-        while not fps.IsPressFinger():
+        while not self.fps.IsPressFinger():
             print('Place finger on scanner.')
             FPS.delay(1)
         print('Thank you for touching me.')
         print('Capturing fingerprint.')
-        fps.CaptureFinger(True)
-        self._image = fps.GetImage()
-        fps.SetLED(False)
+        self.fps.CaptureFinger(True)
+        self._image = self.fps.GetImage()
+        self.fps.SetLED(False)
+
+    def finger_enroll(self):
+        self.fps.SetLED(True)
+        self._finger_number = self.fps.GetEnrollCount()
+        self.fps.EnrollStart(self._finger_number)
+        self.fps.Enroll1()
+        self.fps.Enroll2()
+        self.fps.Enroll3()
+        self.fps.SetLED(False)
+
+    def finger_identify(self):
+        self.fps.SetLED(True)
+        self.fps.CaptureFinger(True)
+        for i in range (5):
+            self._finger_scan_number[i] = self.fps.Identify1_N()
+        self._collected_scans = Counter(self._finger_scan_number)
+        self._true_scan_number = self._collected_scans.most_common(1)
+        return self._true_scan_number
 
 if __name__ == "__main__":
     test_fps = FingerPrintScanner()
